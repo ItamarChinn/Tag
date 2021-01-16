006d1ad0364897d324d6b3f6c80dac2f3ddc56cd
Sarah Edwards <edwardss@mit.edu>
2021-01-16 12:04:42 -0700

    made js documents for necessary components

---
+++ skeleton-master/client/src/components/modules/JourneyCard.css
@@ -0,0 +1,21 @@
+﻿import React, { Component } from "react";
+import "./NewComponent.css";
+
+/**
+ * What is this>
+ */
+class NewComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="NewComponent-container">
+          This is a test component
+      </div>
+    );
+  }
+};
+
+export default NewComponent;
