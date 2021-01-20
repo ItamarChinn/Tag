5b25adbd57ea9eed12362dde4f5cc0aff30966b3
itamarchinn <itamar@chinn.com>
2021-01-20 14:48:32 +0200

    Spline bunny trial

--- client/src/components/modules/JourneyDiagram.js
+++ client/src/components/modules/JourneyDiagram.js
@@ -36,37 +36,41 @@ class JourneyDiagram extends Component {
 
   render() {
     return (
+      // <>
+      //   <svg viewBox="0 0 400 400" width="45%" height="auto">
+      //     <VictoryPie
+      //       standalone={false}
+      //       animate={{ duration: 1000 }}
+      //       width={400} height={400}
+      //       data={this.state.data}
+      //       innerRadius={120}
+      //       cornerRadius={25}
+      //       labels={() => null}
+      //       style={{
+      //         data: { fill: ({ datum }) => {
+      //           const color = datum.y > 30 ? "green" : "red";
+      //           return datum.x === 1 ? color : "darkblue";
+      //         }
+      //         }
+      //       }}
+      //     />
+      //     <VictoryAnimation duration={1000} data={this.state}>
+      //       {(newProps) => {
+      //         return (
+      //           <VictoryLabel
+      //             textAnchor="middle" verticalAnchor="middle"
+      //             x={200} y={200}
+      //             text={`${Math.round(newProps.percent)}%`}
+      //             style={{ fill:"white", fontSize: 45 }}
+      //           />
+      //         );
+      //       }}
+      //     </VictoryAnimation>
+      //   </svg>
+      // </>
+
       <>
-        <svg viewBox="0 0 400 400" width="45%" height="auto">
-          <VictoryPie
-            standalone={false}
-            animate={{ duration: 1000 }}
-            width={400} height={400}
-            data={this.state.data}
-            innerRadius={120}
-            cornerRadius={25}
-            labels={() => null}
-            style={{
-              data: { fill: ({ datum }) => {
-                const color = datum.y > 30 ? "green" : "red";
-                return datum.x === 1 ? color : "darkblue";
-              }
-              }
-            }}
-          />
-          <VictoryAnimation duration={1000} data={this.state}>
-            {(newProps) => {
-              return (
-                <VictoryLabel
-                  textAnchor="middle" verticalAnchor="middle"
-                  x={200} y={200}
-                  text={`${Math.round(newProps.percent)}%`}
-                  style={{ fill:"white", fontSize: 45 }}
-                />
-              );
-            }}
-          </VictoryAnimation>
-        </svg>
+        <iframe src='https://my.spline.design/characterbunny-86db51a8f1186679e73f738622170256/' frameborder='0' width='100%' height='100%'></iframe>
       </>
     );
   }
