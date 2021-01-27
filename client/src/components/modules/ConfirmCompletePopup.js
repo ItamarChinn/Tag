30ec632831cef1574c652ec4446bf7ff4e2d865a
itamarchinn <itamar@chinn.com>
2021-01-27 17:16:40 +0200

    Cant remember what I changed

--- client/src/components/modules/ConfirmCompletePopup.js
+++ client/src/components/modules/ConfirmCompletePopup.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./ConfirmCompletePopup.css";
-import {get, post} from "../../utilities";
+import { get, post } from "../../utilities";
 
 
 /**
@@ -12,7 +12,7 @@ import {get, post} from "../../utilities";
 
 class ConfirmCompletePopup extends Component {
   constructor(props) {
-      super(props);
+    super(props);
   }
 
   markAsCompleteJourney = () => {
@@ -20,36 +20,36 @@ class ConfirmCompletePopup extends Component {
     this.props.closePopup();
   }
 
-    render() {   
-        return (
-        <div className="ConfirmCompletePopup-popup">
-            <div className='ConfirmCompletePopup-popupinner'> 
-            <div className='ConfirmCompletePopup-content'>
-              {(!this.props.completed) ? 
+  render() {
+    return (
+      <div className="ConfirmCompletePopup-popup">
+        <div className='ConfirmCompletePopup-popupinner'>
+          <div className='ConfirmCompletePopup-content'>
+            {(!this.props.completed) ? 
               <>
               <center>
-            Are you sure you wish to mark this journey as complete? You can always revert this decision later if you want to continue on your journey
+                  Are you sure you wish to mark this journey as complete? You can always revert this decision later if you want to continue on your journey.<br/> Referesh to see your changes.
             </center> 
             <div className = "ConfirmCompletePopup-buttonContainer">
             <div className="ConfirmCompletePopup-button2" onClick={this.props.closePopup}>&nbsp; Cancel &nbsp;</div>
             <div className="ConfirmCompletePopup-button" onClick={this.markAsCompleteJourney}>&nbsp; Complete &nbsp;</div>
           </div>
           </>
-            : 
+            :
             <>
             <center>
-            Are you sure you wish to mark this journey as incomplete? You can always revert this decision later if you want to continue on your journey
-            </center> 
-            <div className = "ConfirmCompletePopup-buttonContainer">
+              Are you sure you wish to mark this journey as incomplete? You can always revert this decision later if you want to end your journey again. <br/> Referesh to see your changes.
+            </center>
+          <div className="ConfirmCompletePopup-buttonContainer">
             <div className="ConfirmCompletePopup-button2" onClick={this.props.closePopup}>&nbsp; Cancel &nbsp;</div>
             <div className="ConfirmCompletePopup-button" onClick={this.markAsCompleteJourney}>&nbsp; Mark as incomplete &nbsp;</div>
           </div>
           </>
             }
-              
-            </div>
-            </div>
-          </div>
+
+      </div>
+            </div >
+          </div >
         );
   }
 };
